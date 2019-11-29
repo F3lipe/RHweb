cea5eb31579b549550224c54bbd62cfa75ed1017
Aluno <Aluno@LAB02-PC12>
2019-11-29 11:24:33 -0200

    123

---
+++ RHweb/build/classes/br/edu/ifpb/esperanca/daw2/RecursosHidricos/rs/AgriTechApplicationRS.java
@@ -0,0 +1,11 @@
+package rs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class AgriTechApplicationRS extends Application {
+
+
+
+}
--- RHweb/src/main/java/br/edu/ifpb/esperanca/daw2/RecursosHidricos/entities/Irrigacao.java
+++ RHweb/src/main/java/br/edu/ifpb/esperanca/daw2/RecursosHidricos/entities/Irrigacao.java
@@ -1,4 +1,5 @@
 package br.edu.ifpb.esperanca.daw2.RecursosHidricos.entities;
+
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -8,40 +9,76 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class Irrigacao implements Identificavel {
-	
+
 	@Id
 	private Long id;
 	private Double tempoIrrigacao;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date diasDeIrrigacao;
+
+	private Double umidade;
+
+	private Double temperatura;
+
+	public Double getUmidade() {
+		return umidade;
+	}
+
+	public void setUmidade(Double umidade) {
+		this.umidade = umidade;
+	}
+
+	public Double getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(Double temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public Irrigacao(Double umidade, Double temperatura) {
+		super();
+		this.umidade = umidade;
+		this.temperatura = temperatura;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Double getTempoIrrigacao() {
 		return tempoIrrigacao;
 	}
+
 	public void setTempoIrrigacao(Double tempoIrrigacao) {
 		this.tempoIrrigacao = tempoIrrigacao;
 	}
+
 	public Date getDiasDeIrrigacao() {
 		return diasDeIrrigacao;
 	}
+
 	public void setDiasDeIrrigacao(Date diasDeIrrigacao) {
 		this.diasDeIrrigacao = diasDeIrrigacao;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((diasDeIrrigacao == null) ? 0 : diasDeIrrigacao.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((temperatura == null) ? 0 : temperatura.hashCode());
 		result = prime * result + ((tempoIrrigacao == null) ? 0 : tempoIrrigacao.hashCode());
+		result = prime * result + ((umidade == null) ? 0 : umidade.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -61,30 +98,40 @@ public class Irrigacao implements Identificavel {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (temperatura == null) {
+			if (other.temperatura != null)
+				return false;
+		} else if (!temperatura.equals(other.temperatura))
+			return false;
 		if (tempoIrrigacao == null) {
 			if (other.tempoIrrigacao != null)
 				return false;
 		} else if (!tempoIrrigacao.equals(other.tempoIrrigacao))
 			return false;
+		if (umidade == null) {
+			if (other.umidade != null)
+				return false;
+		} else if (!umidade.equals(other.umidade))
+			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Irrigacao [id=" + id + ", tempoIrrigacao=" + tempoIrrigacao + ", diasDeIrrigacao=" + diasDeIrrigacao
-				+ "]";
+				+ ", umidade=" + umidade + ", temperatura=" + temperatura + "]";
 	}
+
 	public Irrigacao(Long id, Double tempoIrrigacao, Date diasDeIrrigacao) {
 		super();
 		this.id = id;
 		this.tempoIrrigacao = tempoIrrigacao;
 		this.diasDeIrrigacao = diasDeIrrigacao;
 	}
+
 	public Irrigacao() {
 		super();
-		
+
 	}
-	
-	
-	
-	
+
 }
